585126a3806b90e1b13a1359685c8843e5a1e335
monteirovictor <victormonteiromaia95@gmail.com>
2021-07-03 22:34:41 -0300

    Create plot

--- Lista 2/atividade2.py
+++ Lista 2/atividade2.py
@@ -2,6 +2,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df= pd.read_csv("Lista 2/Dados.csv", names=['time','x','y','z','index'])
 
-data_frame= pd.read_csv("Lista 2/arquivo.csv")
-print(data_frame)
+#legendas
+titulo="Acelerometro"
+eixox="Eixo X"
+
+#legendas
+plt.title(titulo)
+plt.xlabel(eixox)
+
+#Plotagem
+plt.plot(df['time'],df['x'],label="Amostra de X")
+plt.legend()
+plt.show()
+
