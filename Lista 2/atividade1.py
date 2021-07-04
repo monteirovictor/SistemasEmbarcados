6c6153bc28eb88ea090e61507d867e324e912bb0
monteirovictor <victormonteiromaia95@gmail.com>
2021-07-03 23:39:00 -0300

    03-07-2021

--- Lista 2/atividade1.py
+++ Lista 2/atividade1.py
@@ -1,3 +1,16 @@
 #Encontre em um texto os nomes próprios e os retorne em uma lista.
 #Utilize o Regex (‘import re’) e a função findall(). 
 #Na versão básica, retorne todas as palavras que iniciam com maiúscula.
+
+import re
+
+texto='Victor rangel,Gabriel maia'
+padrao=[]
+
+padrao = re.findall(r'[A-Z][a-z]+',texto)
+
+if padrao:
+    print(padrao)
+else:
+    print("Não existe nomes próprios")
+    
