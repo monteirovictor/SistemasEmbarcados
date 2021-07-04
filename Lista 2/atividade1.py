b89f35d7e5d224f9cf407332a0412f538638e35e
monteirovictor <victormonteiromaia95@gmail.com>
2021-07-03 22:59:38 -0300

    create file 1

---
+++ Lista 2/atividade1.py
@@ -0,0 +1,3 @@
+#Encontre em um texto os nomes próprios e os retorne em uma lista.
+#Utilize o Regex (‘import re’) e a função findall(). 
+#Na versão básica, retorne todas as palavras que iniciam com maiúscula.
